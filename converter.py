32629c98b5060293c69a6688d274dccc9cec6aa1
xmedvenyashax <xmedvenyashax@gmail.com>
2023-09-04 11:36:38 +0300

    v.2.2.3

--- converter.py
+++ converter.py
@@ -7,64 +7,95 @@ import PIL
 import os
 
 
-def check_time(start_time):
+def print_time(start_time):
     elapsed_time = time.time() - start_time
     rounded_time = round(elapsed_time)
     minutes = (rounded_time % 3600) // 60
     seconds = rounded_time % 60
-    return f"Время работы: {minutes} минут, {seconds} секунд(ы)\nКонвертация завершена.\n"
+    return (
+        f"Время работы: {minutes} минут, {seconds} секунд(ы)\n"
+        f"Конвертация завершена.\n"
+    )
 
 
 def check_file(file, from_):
+    """
+    Checking the file for the validity of the format
+    file - This is the file that is being checked
+    from_ - This is what format the conversion will be made from
+    :return True if format matches the selected format
+    """
     if str(file).endswith(from_.lower()) or (str(file).endswith(from_.upper())):
         return True
     return False
 
 
-def format_error(file, from_, in_):
+def print_error(file, from_, in_):
+    """
+    in_ - This is the format to which the conversion will be performed
+    :return - Error description
+    """
     file_format = os.path.splitext(file)[1]
-    return (f'Неверный формат файла. Выбран формат конвертации с {from_} в {in_},'
-            f' у файла в данный момент формат {str(file_format)}') + '\n'
+    return (f"Неверный формат файла. Выбран формат конвертации с {from_} в {in_},"
+            f" у файла в данный момент формат {str(file_format)}") + "\n"
 
 
 class Converter:
+    """Image converter, from avif to png format, or webp to png"""
     def __init__(self, files: tuple, type_files: int, delete_file: str):
+        """
+        files - Files for conversation
+        type_files: 1 - avif to png, 2 - webp to png
+        delete_file: On or off
+        """
         self.files = files
         self.type_file = type_files
         self.delete_file = delete_file
 
     def image_codecs_method(self, file, from_, in_):
+        """The method of converting images using the imagecodecs library"""
         if check_file(file, from_) is True:
             image = imagecodecs.imread(file)
             imagecodecs.imwrite(str(file).replace(from_, in_), image)
             result = self.delete_files(file)
-            return f'Конвертация успешно выполнена\n{result}\n'
+            return f"Конвертация успешно выполнена\n{result}\n"
         else:
-            return format_error(file, from_, in_)
+            return print_error(file, from_, in_)
 
     def pillow_method(self, image, file, from_, in_):
+        """The method of converting images using the pillow_avif library"""
         if check_file(file, from_) is True:
-            image.save(str(file).replace(f'.{from_}', f'.{in_}'))
+            image.save(str(file).replace(f".{from_}", f".{in_}"))
             result = self.delete_files(file)
-            return f'Конвертация успешно выполнена\n{result}\n'
+            return f"Конвертация успешно выполнена\n{result}\n"
         else:
-            return format_error(file, from_, in_)
+            return print_error(file, from_, in_)
 
     def delete_files(self, file):
-        result_messages = 'Исходный файл не удалялся, снята галочка "Удалить исходный файл"'
-        if self.delete_file == 'on':
+        """
+        The function is triggered if
+         the delete source file check box is set
+         """
+        result_messages = (
+            'Исходный файл не удалялся, снята галочка "Удалить исходный файл"'
+        )
+        if self.delete_file == "on":
             try:
                 os.remove(file)
-                return 'Удаление исходного файла успешно выполнено'
-            except:
-                return 'Ошибка удаления исходного файла: ' + file
+                return "Удаление исходного файла успешно выполнено"
+            except (FileExistsError, FileNotFoundError, PermissionError):
+                return "Ошибка удаления исходного файла: " + file
         return result_messages
 
     def convert_file(self, from_, in_):
+        """
+        Main function for conversation files in the desired format
+        :return Result messages
+        """
         result_messages = ""
         for file in self.files:
             if not os.path.exists(file):
-                result_messages += 'Файл не существует: ' + file + '\n'
+                result_messages += "Файл не существует: " + file + "\n"
             else:
                 try:
                     image = Image.open(file)
--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-from converter import Converter, check_time
+from converter import Converter, print_time
 from tkinter import filedialog
 from PIL import Image
 import customtkinter
@@ -6,108 +6,165 @@ import threading
 import tkinter
 import time
 
-customtkinter.set_appearance_mode('Dark')
-customtkinter.set_default_color_theme('dark-blue')
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("dark-blue")
 
 
-class MyFrame(customtkinter.CTkFrame):
+class MainFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.files = None
         self.grid_columnconfigure(1, weight=1)
+        self.files = None
 
-        # main
-        self.text_loger = customtkinter.CTkTextbox(self, corner_radius=0, height=400, width=800)
-        self.text_loger.grid(row=0, column=1, padx=(5, 0), pady=(5, 0))
-        self.text_loger.insert('end', 'Выбранные файлы на конвертацию:\n')
+        # Text box for result or error messages
+        self.text_loger = customtkinter.CTkTextbox(
+            self, corner_radius=0,
+            height=400, width=800
+        )
+        self.text_loger.grid(
+            row=0, column=1,
+            padx=(5, 0), pady=(5, 0)
+        )
+        self.text_loger.insert("end", "Выбранные файлы на конвертацию:\n")
 
-        self.button_open_file = customtkinter.CTkButton(self, text='Выбрать файл(ы)', corner_radius=0,
-                                                        fg_color='#3f4544',
-                                                        text_color='#faf7f7', command=self.open_files)
-        self.button_open_file.grid(row=1, column=1, padx=(50, 0), pady=(8, 0))
+        self.choose_file_button = customtkinter.CTkButton(
+            self, text="Выбрать файл(ы)",
+            corner_radius=0, fg_color="#3f4544",
+            text_color="#faf7f7", command=self.open_files,
+        )
+        self.choose_file_button.grid(
+            row=1, column=1,
+            padx=(50, 0), pady=(8, 0)
+        )
 
-        self.button_convert = customtkinter.CTkButton(self, text='Начать конвертацию', corner_radius=0,
-                                                      command=self.go_to_convert)
-        self.button_convert.grid(row=1, column=1, padx=(350, 0), pady=(8, 0))
+        self.convert_button = customtkinter.CTkButton(
+            self, text="Начать конвертацию",
+            corner_radius=0, command=self.send_for_conversion
+        )
+        self.convert_button.grid(
+            row=1, column=1,
+            padx=(350, 0), pady=(8, 0)
+        )
 
-        self.check_var = customtkinter.StringVar(value="on")
-        checkbox = customtkinter.CTkCheckBox(self, text="Удалить исходный файл",
-                                             variable=self.check_var, onvalue="on", offvalue="off")
-        checkbox.grid(row=1, column=1, padx=(0, 290), pady=(8, 0))
+        self.check_var = customtkinter.StringVar(
+            value="on"
+        )
+        checkbox_for_delete = customtkinter.CTkCheckBox(
+            self, text="Удалить исходный файл",
+            variable=self.check_var,
+            onvalue="on", offvalue="off",
+        )
+        checkbox_for_delete.grid(
+            row=1, column=1,
+            padx=(0, 290), pady=(8, 0)
+        )
 
-        # right bar
-        self.right_bar_frame = customtkinter.CTkFrame(self, corner_radius=0)
-        self.right_bar_frame.grid(row=0, column=2, pady=(6, 0))
+        # right frame
+        self.right_bar_frame = customtkinter.CTkFrame(
+            self, corner_radius=0
+        )
+        self.right_bar_frame.grid(
+            row=0, column=2,
+            pady=(6, 0)
+        )
 
-        self.label_type_convert = customtkinter.CTkLabel(self.right_bar_frame, text_color='#faf7f7', text='Формат:')
-        self.label_type_convert.grid(row=0, column=0, pady=(150, 0))
+        self.label_type_convert = customtkinter.CTkLabel(
+            self.right_bar_frame,
+            text_color="#faf7f7",
+            text="Формат:"
+        )
+        self.label_type_convert.grid(
+            row=0, column=0, pady=(150, 0)
+        )
 
-        self.radio_var = tkinter.IntVar(value=1)
-        self.radiobutton_1 = customtkinter.CTkRadioButton(self.right_bar_frame, text="AVIF в PNG",
-                                                          variable=self.radio_var, value=1)
-        self.radiobutton_2 = customtkinter.CTkRadioButton(self.right_bar_frame, text="WEBP в PNG",
-                                                          variable=self.radio_var, value=2)
-        self.radiobutton_1.grid(row=1, column=0, pady=(0, 0), padx=(5, 5))
-        self.radiobutton_2.grid(row=2, column=0, pady=(8, 170), padx=(5, 5))
+        self.type_file_var = tkinter.IntVar(value=1)
+        self.avif_to_png_button = customtkinter.CTkRadioButton(
+            self.right_bar_frame, text="AVIF в PNG",
+            variable=self.type_file_var, value=1
+        )
+        self.webp_to_png_button = customtkinter.CTkRadioButton(
+            self.right_bar_frame, text="WEBP в PNG",
+            variable=self.type_file_var, value=2
+        )
+        self.avif_to_png_button.grid(
+            row=1, column=0,
+            pady=(0, 0), padx=(5, 5)
+        )
+        self.webp_to_png_button.grid(
+            row=2, column=0,
+            pady=(8, 170), padx=(5, 5)
+        )
 
-    def go_to_convert(self):
-        type_file = self.radio_var.get()
+    def send_for_conversion(self):
+        type_file = self.type_file_var.get()
         delete_file = self.check_var.get()
         if self.files is not None:
             converter = Converter(self.files, type_file, delete_file)
 
-            def convert_and_get_log():
+            def send():
                 start_time = time.time()
-                self.text_loger.insert('end', '\nПроцесс конвертации начался...\n')
+                self.text_loger.insert("end", "\nПроцесс конвертации начался...\n")
                 if type_file == 1 and len(self.files) != 0:
-                    avif_convert = converter.convert_file('avif', 'png')
-                    self.text_loger.insert('end', avif_convert)
-                    time_convect = check_time(start_time)
-                    self.text_loger.insert('end', time_convect)
+                    avif_convert = converter.convert_file("avif", "png")
+                    self.text_loger.insert("end", avif_convert)
+                    time_convect = print_time(start_time)
+                    self.text_loger.insert("end", time_convect)
                 elif type_file == 2 and len(self.files) != 0:
-                    webp_convert = converter.convert_file('webp', 'png')
-                    self.text_loger.insert('end', webp_convert)
-                    time_convect = check_time(start_time)
-                    self.text_loger.insert('end', time_convect)
+                    webp_convert = converter.convert_file("webp", "png")
+                    self.text_loger.insert("end", webp_convert)
+                    time_convect = print_time(start_time)
+                    self.text_loger.insert("end", time_convect)
                 else:
-                    self.text_loger.insert('end', 'Файл(ы) для конвертации не выбран(ы)\n')
+                    self.text_loger.insert(
+                        "end", "Файл(ы) для конвертации не выбран(ы)\n"
+                    )
 
-            threading.Thread(target=convert_and_get_log).start()
-
+            threading.Thread(target=send).start()
         else:
-            self.text_loger.insert('end', 'Файл(ы) для конвертации не выбран(ы)\n')
+            self.text_loger.insert("end", "Файл(ы) для конвертации не выбран(ы)\n")
 
     def open_files(self):
+        """Function for selecting files"""
         self.files = filedialog.askopenfilenames()
-        text = 'Выбранные файлы на конвертацию:'
+        text = "Выбранные файлы на конвертацию:"
         if len(self.files) != 0:
-            self.text_loger.delete('1.0', 'end')
-            print_file_path = text + str(self.files).replace(',', ',\n') + '\n'
-            self.text_loger.insert('1.0', text=print_file_path)
+            self.text_loger.delete("1.0", "end")
+            print_file_path = text + str(self.files).replace(",", ",\n") + "\n"
+            self.text_loger.insert("1.0", text=print_file_path)
         else:
-            text = 'Выбранные файлы на конвертацию:'
-            self.text_loger.delete('1.0', 'end')
-            self.text_loger.insert('end', text)
+            text = "Выбранные файлы на конвертацию:"
+            self.text_loger.delete("1.0", "end")
+            self.text_loger.insert("end", text)
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.logo_image = customtkinter.CTkImage(Image.open('./icon/logo.ico'), size=(26, 26))
+        self.logo_image = customtkinter.CTkImage(
+            Image.open("./icon/logo.ico"),
+            size=(26, 26)
+        )
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.title('Одна секунда конвертер')
-        self.my_frame = MyFrame(master=self, height=450, width=800, corner_radius=0, fg_color="transparent")
-        self.my_frame.grid(row=0, column=0, sticky="nsew")
+        self.title("One second converter")
+        self.my_frame = MainFrame(
+            master=self, height=450,
+            width=800, corner_radius=0,
+            fg_color="transparent"
+        )
+        self.my_frame.grid(
+            row=0, column=0,
+            sticky="nsew"
+        )
 
 
-if __name__ == '__main__':
-    width = 800
+if __name__ == "__main__":
+    width = 600
     height = 450
-    x = 560
+    x = 700
     y = 260
     app = App()
-    app.iconbitmap('./icon/logo.ico')
-    app.geometry(f'{width}x{height}+{x}+{y}')
+    app.iconbitmap("./icon/logo.ico")
+    app.geometry(f"{width}x{height}+{x}+{y}")
     app.resizable(False, False)
     app.mainloop()
